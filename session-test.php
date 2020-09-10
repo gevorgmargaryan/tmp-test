4ccb41e4bb75a64a42f06c79fe3a4cee0c52326f
Gevorg Margaryan <gevorgmargaryan@yahoo.com>
2020-09-10 19:44:54 +0400

    Update session-test.php

--- session-test.php
+++ session-test.php
@@ -7,13 +7,13 @@ ini_set('session.save_handler', 'memcached');
 ini_set('session.save_path', $node_endpoint);
 
 ini_set('memcache.allow_failover',1);
-ini_set('memcache.session_redundancy', 4);
+ini_set('memcache.session_redundancy', 2);
 
 header('Content-Type: text/plain');
 
 session_start();
 if (!isset($_SESSION['visit'])) {
-    echo 'This is the first time you\'re visiting this server\n';
+    echo "This is the first time you\'re visiting this server\n";
     $_SESSION['visit'] = 0;
 } else
     echo 'Your number of visits: '.$_SESSION['visit'] . PHP_EOL;
@@ -24,9 +24,12 @@ $_SESSION['visit']++;
 // echo 'Client IP: '.$_SERVER['REMOTE_ADDR'] . '\n';
 
 print_r($_COOKIE);
+
+echo "\n>>> node setup";
 print_r($_SESSION);
 session_write_close();
-echo "\n>>>trying with config endpoint\n";
+
+echo "\n>>> trying with config endpoint\n";
 ini_set('session.save_path', $conf_endpoint);
 
 session_start();
